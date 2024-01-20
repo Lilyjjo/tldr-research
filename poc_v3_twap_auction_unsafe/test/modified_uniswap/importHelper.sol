5cb970b3aa9fa4df468d2ea8857eb6043a68738e
Lily Johnson <lillianjanjohnson@gmail.com>
2024-01-20 09:56:17 -0500

    [refactor] changed poc dir into testing dir

--- poc_v3_twap_auction_unsafe/test/modified_uniswap/importHelper.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.7.0;
-
-// Note: is a helper contract to help with mismatched compiler versions.
-import {UniswapV3Factory} from "v3-core/UniswapV3Factory.sol";
-import {UniswapV3Pool} from "v3-core/UniswapV3Pool.sol";
-
-contract Empty {}
