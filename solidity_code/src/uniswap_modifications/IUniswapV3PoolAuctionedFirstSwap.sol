bd28491cdeb7b382a5aa99c33065b72b50de4042
Lily Johnson <lillianjanjohnson@gmail.com>
2024-05-01 10:49:54 -0400

    refactor(uniswap): start code clean

--- solidity_code/src/uniswap_modifications/IUniswapV3PoolAuctionedFirstSwap.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.8;
-
-import {IUniswapV3Pool} from "v3-core/interfaces/IUniswapV3Pool.sol";
-import {IAuctionGuard} from "../interfaces/IAuctionGuard.sol";
-
-interface IUniswapV3PoolAuctionedFirstSwap is IUniswapV3Pool, IAuctionGuard {}
